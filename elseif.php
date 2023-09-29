196659f023edf4e0e73974a2be94cc3fffab9b15
Dudabuen0 <130669340+Dudabuen0@users.noreply.github.com>
2023-09-29 09:12:56 -0300

    Commit Da Maria

--- elseif.php
+++ elseif.php
@@ -7,13 +7,31 @@
 </head>
 <body>
     <?php
+    $numero1 = 5;
+    $numero2 = 2;
+    $nome1 = 'Joaõ';
+    $nome2 = 'Robson';
+    $numero3 = 12;
+    $numero4 = 11;
 
-    if (5>2){
-        echo "Exibe esse texto se a expressão for verdadeira <br>";
+    //Verifica se $numero1 é maior que $numero2.
+    if ($numero1 > $numero2){
+        echo "A expressão é verdadeira! <br>";
     }else{
-        echo "Exibe esse texto quando a expressão for falsa <br>";
+        echo "A expressão é falsa! <br>";
     }
-    
+    //Verifica se $nome1 é diferente de $nome2.
+    if ($nome1 != $nome2){
+        echo "João é diferente de Robson! <br>";
+    }else{
+        echo "João é igual a Robson!";
+    }
+    //Verifica se $numero3 é maior ou igual a $numero4.
+    if ($numero3 <= $numero4){
+        echo "12 é maior que 11! <br>";
+    }else{
+        echo "12 não é maior, é igual a 11! <br>";
+    }
     ?>
 </body>
 </html>
