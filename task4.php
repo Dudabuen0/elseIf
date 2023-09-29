b0d58ac0329ce7b736a08eb5255f19c0a01434c3
Dudabuen0 <130669340+Dudabuen0@users.noreply.github.com>
2023-09-29 13:45:11 -0300

    Commit Da Maria

---
+++ task4.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //Declaração de variaveis
+        $velocidade = 80;
+
+        //Verifica velocidade 
+        if ($velocidade >= 41 && $velocidade <= 50 ){
+            echo "Você está na velocidade permitida!";
+        }elseif ($velocidade < 40 ){
+            echo "Atenção você está muito devagar!";
+        }elseif ($velocidade > 50){
+            echo "Você ultrapassou a velocidade limite, multado!";
+        }
+    ?>
+</body>
+</html>
