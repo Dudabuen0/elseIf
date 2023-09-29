83110ec590279014ccd160ca6b963734c7369c5b
Dudabuen0 <130669340+Dudabuen0@users.noreply.github.com>
2023-09-29 14:30:19 -0300

    Commit Da Maria

---
+++ swith.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+    // declaração das variáveis
+    $numero = 5;
+    $nome = "";
+
+    // lê a variável
+    switch($numero) {
+
+
+    // compara a variável
+    case 4:
+        echo "O número é igual a 4 <br>";
+    break;
+
+    // compara a variável
+    case 5:
+        echo "O número é igual a 5 <br>";
+    break;
+
+    // valor nulo
+    default:
+    echo "O número não corresponde aos valores <br>";
+}
+
+    // lê a variável 
+    switch($nome) {
+
+    // compara a variável
+    case "Rosana": 
+        echo "O nome é Rosana"; 
+    break;
+
+    // compara a variável 
+    case "Isis": 
+        echo "O nome é Isis"; 
+    break;
+
+    // valor nulo
+    default:
+        echo "O nome não foi encontrado";
+}
+
+?>
+</body>
+</html>
