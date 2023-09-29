e5b3523e134c10bcd7d5914ee0a80c3550369752
Dudabuen0 <130669340+Dudabuen0@users.noreply.github.com>
2023-09-29 13:58:47 -0300

    Commit Da Maria

--- task4.php
+++ task4.php
@@ -8,12 +8,12 @@
 <body>
     <?php
         //Declaração de variaveis
-        $velocidade = 80;
+        $velocidade = 40;
 
         //Verifica velocidade 
         if ($velocidade >= 41 && $velocidade <= 50 ){
             echo "Você está na velocidade permitida!";
-        }elseif ($velocidade < 40 ){
+        }elseif ($velocidade <= 40 ){
             echo "Atenção você está muito devagar!";
         }elseif ($velocidade > 50){
             echo "Você ultrapassou a velocidade limite, multado!";
