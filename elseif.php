40aae6a64a8df2a1bd597c2a7f60f7a274810b4f
Dudabuen0 <130669340+Dudabuen0@users.noreply.github.com>
2023-09-29 08:51:23 -0300

    Commit da Maria

---
+++ elseif.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>If e Else</title>
+</head>
+<body>
+    <?php
+
+    if (5>2){
+        echo "Exibe esse texto se a expressão for verdadeira <br>";
+    }else{
+        echo "Exibe esse texto quando a expressão for falsa <br>";
+    }
+    
+    ?>
+</body>
+</html>
